a39d2dcf497a6fd4218091fe6d1fec8cfd755113
Heitor Galassi <hgalassi17@gmail.com>
2026-02-05 02:01:25 -0300

    Adding save method

--- src/galassi/fun/Main.java
+++ src/galassi/fun/Main.java
@@ -1,17 +1,13 @@
 package galassi.fun;
 
-import galassi.fun.payment.decorator.MakePayment;
-import galassi.fun.payment.decorator.PaymentByCreditCard;
-import galassi.fun.payment.decorator.PaymentInfo;
-import galassi.fun.payment.decorator.PaymentOffersLoan;
+import galassi.fun.payment.decorator.*;
 
 public class Main {
     public static void main(String[] args) {
         PaymentInfo paymentInfo = new PaymentInfo(200.00D, 350.00D);
         MakePayment makePayment = new PaymentByCreditCard(paymentInfo);
         makePayment = new PaymentOffersLoan(makePayment, true, paymentInfo);
+        makePayment = new SavePaymentInMemory(makePayment);
         makePayment.executePayment();
-
-
     }
 }
